d95e338604f7d71ce8cbc6cb3a4f707f45cfd6cb
NelsonBeard <ilnur111111yandex.ru>
2022-02-03 00:24:58 +0500

    Создал экран с фото дня

--- app/src/main/java/com/geekbrains/pictureoftheday/MainActivity.kt
+++ app/src/main/java/com/geekbrains/pictureoftheday/MainActivity.kt
@@ -2,10 +2,16 @@ package com.geekbrains.pictureoftheday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geekbrains.pictureoftheday.ui.picture.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/com/geekbrains/pictureoftheday/ui/picture/PictureOfTheDayFragment.kt
@@ -0,0 +1,70 @@
+package com.geekbrains.pictureoftheday.ui.picture
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import coil.load
+import com.geekbrains.pictureoftheday.R
+import kotlinx.android.synthetic.main.picture_of_the_day_fragment.*
+
+class PictureOfTheDayFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+        private var isMain = true
+    }
+
+    private lateinit var viewModel: PictureOfTheDayViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.picture_of_the_day_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[PictureOfTheDayViewModel::class.java]
+        viewModel.getData().observe(viewLifecycleOwner,{ renderData(it) })
+
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    toast("Link is empty")
+                } else {
+                    image_view.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                //Nothing to do
+            }
+            is PictureOfTheDayData.Error -> {
+                toast(data.error.message)
+            }
+        }
+    }
+
+    private fun Fragment.toast(string: String?) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.BOTTOM, 0, 250)
+            show()
+        }
+    }
+
+}
